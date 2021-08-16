14c59fd0f0272818e0905e52e6bae7445ab3c882
Vladimir Sadovnikov <sadko4u@gmail.com>
2021-08-17 00:54:26 +0300

    Updated dependencies

--- project.mk
+++ project.mk
@@ -21,7 +21,7 @@
 # Package version
 ARTIFACT_ID                 = LSP_PLUGINS_GRAPH_EQUALIZER
 ARTIFACT_NAME               = lsp-plugins-graph-equalizer
-ARTIFACT_DESC               = Trigger plugin series
+ARTIFACT_DESC               = Graphic Equalizer plugin series
 ARTIFACT_HEADERS            = lsp-plug.in
 ARTIFACT_EXPORT_ALL         = 1
 ARTIFACT_VERSION            = 1.0.3-devel
